e986d8c0c97bc54b3e0871bf28001eda195ad77f
dwptra <muhamaddwiputranovriansyah@gmail.com>
2025-07-22 22:52:03 +0700

    menambah data di seeder jadi banyak

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -19,5 +19,77 @@ class BarangSeeder extends Seeder
             "kode_suplier" => "S001",
             "nama_suplier" => "Toko Buku ABC",
         ]);
+        BarangModel::create([
+            "nama_barang" => "buku tulis",
+            "kode_barang" => "B001",
+            "jumlah_barang" => 100,
+            "harga_dasar" => 5000.00,
+            "harga_jual" => 7000.00,
+            "kode_suplier" => "S001",
+            "nama_suplier" => "Toko Buku ABC",
+        ]);
+        BarangModel::create([
+            "nama_barang" => "buku tulis",
+            "kode_barang" => "B001",
+            "jumlah_barang" => 100,
+            "harga_dasar" => 5000.00,
+            "harga_jual" => 7000.00,
+            "kode_suplier" => "S001",
+            "nama_suplier" => "Toko Buku ABC",
+        ]);
+        BarangModel::create([
+            "nama_barang" => "buku tulis",
+            "kode_barang" => "B001",
+            "jumlah_barang" => 100,
+            "harga_dasar" => 5000.00,
+            "harga_jual" => 7000.00,
+            "kode_suplier" => "S001",
+            "nama_suplier" => "Toko Buku ABC",
+        ]);
+        BarangModel::create([
+            "nama_barang" => "buku tulis",
+            "kode_barang" => "B001",
+            "jumlah_barang" => 100,
+            "harga_dasar" => 5000.00,
+            "harga_jual" => 7000.00,
+            "kode_suplier" => "S001",
+            "nama_suplier" => "Toko Buku ABC",
+        ]);
+        BarangModel::create([
+            "nama_barang" => "buku tulis",
+            "kode_barang" => "B001",
+            "jumlah_barang" => 100,
+            "harga_dasar" => 5000.00,
+            "harga_jual" => 7000.00,
+            "kode_suplier" => "S001",
+            "nama_suplier" => "Toko Buku ABC",
+        ]);
+        BarangModel::create([
+            "nama_barang" => "buku tulis",
+            "kode_barang" => "B001",
+            "jumlah_barang" => 100,
+            "harga_dasar" => 5000.00,
+            "harga_jual" => 7000.00,
+            "kode_suplier" => "S001",
+            "nama_suplier" => "Toko Buku ABC",
+        ]);
+        BarangModel::create([
+            "nama_barang" => "buku tulis",
+            "kode_barang" => "B001",
+            "jumlah_barang" => 100,
+            "harga_dasar" => 5000.00,
+            "harga_jual" => 7000.00,
+            "kode_suplier" => "S001",
+            "nama_suplier" => "Toko Buku ABC",
+        ]);
+        BarangModel::create([
+            "nama_barang" => "buku tulis",
+            "kode_barang" => "B001",
+            "jumlah_barang" => 100,
+            "harga_dasar" => 5000.00,
+            "harga_jual" => 7000.00,
+            "kode_suplier" => "S001",
+            "nama_suplier" => "Toko Buku ABC",
+        ]);
     }
 }
